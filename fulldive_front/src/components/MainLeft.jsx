b591afa45ef26cbc20394dc5b55523db8f6fa2b2
muse9312 <muse9312@naver.com>
2023-03-06 16:09:32 +0900

    main refactoring

---
+++ fulldive_front/src/components/MainLeft.jsx
@@ -0,0 +1,238 @@
+import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+// import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { Route, Routes } from 'react-router-dom';
+const menus = [
+  {
+    id: 1,
+    name: "홈",
+    imgUrl: "/images/house.svg",
+    nowImgUrl: "/images/house_white.svg",
+    pathname: "/",
+  },
+  {
+    id: 2,
+    name: "스테이지",
+    imgUrl: "/images/stage.svg",
+    nowImgUrl: "/images/stage_white.svg",
+    pathname: "/stage/liveStage",
+  },
+  {
+    id: 3,
+    name: "아티스트",
+    imgUrl: "/images/artist.svg",
+    nowImgUrl: "/images/artist_white.svg",
+    pathname: "/artist",
+  },
+  // {
+  //   id: 4,
+  //   name: "스토어",
+  //   imgUrl: "/images/shop.svg",
+  //   nowImgUrl: "/images/shop_white.svg",
+  //   pathname: "/store",
+  // },
+  {
+    id: 5,
+    name: "설정",
+    imgUrl: "/images/setting.svg",
+    nowImgUrl: "/images/setting_white.svg",
+    pathname: "/setting/profile",
+  },
+  {
+    id: 6,
+    name: "도움말",
+    imgUrl: "/images/flag.svg",
+    nowImgUrl: "/images/flag_white.svg",
+    pathname: "/help",
+  },
+];
+
+const company_setting_menus = [
+  {
+    id: 1,
+    name: "프로필",
+    imgUrl: "/images/setting/user.svg",
+    nowImgUrl: "/images/setting/user_white.svg",
+    pathname: "/setting/profile",
+  },
+  // {
+  //   id: 2,
+  //   name: "애널리틱스",
+  //   imgUrl: "/images/setting/analytics.svg",
+  //   nowImgUrl: "/images/setting/analytics_white.svg",
+  //   pathname: "/setting/analytics",
+  // },
+  {
+    id: 2,
+    name: "스튜디오",
+    imgUrl: "/images/setting/studio.svg",
+    nowImgUrl: "/images/setting/studio_white.svg",
+    pathname: "/setting/studio",
+  },
+  {
+    id: 3,
+    name: "수익정산",
+    imgUrl: "/images/setting/coin.svg",
+    nowImgUrl: "/images/setting/coin_white.svg",
+    pathname: "/setting/coin",
+  },
+];
+
+const artist_setting_menus = [
+  {
+    id: 1,
+    name: "프로필",
+    imgUrl: "/images/setting/user.svg",
+    nowImgUrl: "/images/setting/user_white.svg",
+    pathname: "/setting/profile",
+  },
+  // {
+  //   id: 2,
+  //   name: "애널리틱스",
+  //   imgUrl: "/images/setting/analytics.svg",
+  //   nowImgUrl: "/images/setting/analytics_white.svg",
+  //   pathname: "/setting/analytics",
+  // },
+  {
+    id: 2,
+    name: "스튜디오",
+    imgUrl: "/images/setting/studio.svg",
+    nowImgUrl: "/images/setting/studio_white.svg",
+    pathname: "/setting/studio",
+  },
+  {
+    id: 3,
+    name: "수익정산",
+    imgUrl: "/images/setting/coin.svg",
+    nowImgUrl: "/images/setting/coin_white.svg",
+    pathname: "/setting/coin",
+  },
+];
+
+const user_setting_menus = [
+  {
+    id: 1,
+    name: "프로필",
+    imgUrl: "/images/setting/user.svg",
+    nowImgUrl: "/images/setting/user_white.svg",
+    pathname: "/setting/userProfile",
+  },
+  {
+    id: 2,
+    name: "보관함",
+    imgUrl: "/images/setting/studio.svg",
+    nowImgUrl: "/images/setting/studio_white.svg",
+    pathname: "/setting/package",
+  },
+  {
+    id: 3,
+    name: "팔로잉",
+    imgUrl: "/images/setting/following.svg",
+    nowImgUrl: "/images/setting/following_white.svg",
+    pathname: "/setting/following",
+  },
+  {
+    id: 4,
+    name: "결제",
+    imgUrl: "/images/setting/coin.svg",
+    nowImgUrl: "/images/setting/coin_white.svg",
+    pathname: "/setting/coin",
+  },
+];
+
+const MainLeft = () => {
+  const router = Routes();
+  const { pathname } = router;
+  const pathname_split = pathname.split("/")[1];
+  const setting_pathname_split = pathname.split("/")[2];
+  const token = useSelector((state) => state.Auth.token);
+  const admin = useSelector((state) => state.Auth.admin);
+
+  return (
+    <>
+      {token === "Test" && pathname_split === "setting" ? (
+        admin === "2" ? (
+          <Nav>
+            {user_setting_menus.map((menu, index) => (
+              <Link href={menu.pathname} key={index} legacyBehavior passHref>
+                <NavInfo
+                  key={index}
+                  imgUrl={menu.imgUrl}
+                  isHere={setting_pathname_split === menu.pathname.split("/")[2]}
+                  nowImgUrl={menu.nowImgUrl}
+                >
+                  {menu.name}
+                </NavInfo>
+              </Link>
+            ))}
+          </Nav>
+        ) : (
+          <Nav>
+            {company_setting_menus.map((menu, index) => (
+              <Link href={menu.pathname} key={index} legacyBehavior passHref>
+                <NavInfo
+                  key={index}
+                  imgUrl={menu.imgUrl}
+                  isHere={setting_pathname_split === menu.pathname.split("/")[2]}
+                  nowImgUrl={menu.nowImgUrl}
+                >
+                  {menu.name}
+                </NavInfo>
+              </Link>
+            ))}
+          </Nav>
+        )
+      ) : (
+        <Nav>
+          {menus.map((menu, index) => (
+            <Link href={menu.pathname} key={index} legacyBehavior passHref>
+              <NavInfo key={index} imgUrl={menu.imgUrl} isHere={pathname_split === menu.pathname.split("/")[1]} nowImgUrl={menu.nowImgUrl}>
+                {menu.name}
+              </NavInfo>
+            </Link>
+          ))}
+        </Nav>
+      )}
+    </>
+  );
+};
+
+const Nav = styled.nav`
+  position: fixed;
+  padding-top: 100px;
+  min-width: 250px;
+  height: 100%;
+  background: #181820;
+  padding: 10px 30px;
+  color: #878787;
+  display: flex;
+  flex-direction: column;
+`;
+
+const NavInfo = styled.a`
+  display: inline-block;
+  min-height: 40px;
+  margin-bottom: 30px;
+  background-position: 0 50%;
+  background-size: contain;
+  padding-left: 40px;
+  line-height: 40px;
+  ${(props) =>
+    props.isHere
+      ? `
+      color: #ffffff;
+      background: url(${props.nowImgUrl}) no-repeat left 50%;
+  `
+      : `
+      color: #8B8B8B;
+      background: url(${props.imgUrl}) no-repeat left 50%;;
+  `}
+  :hover {
+    color: #ffffff;
+    ${(props) => `background: url(${props.nowImgUrl}) no-repeat left 50%;`}
+  }
+`;
+
+export default MainLeft;
---
+++ fulldive_front/src/components/MainTop.jsx
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+import React from "react";
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const MainTop = () => {
+  const token = useSelector((state) => state.Auth.token);
+
+  const onClickLogout = () => {
+    window.localStorage.clear();
+    window.location.replace("/");
+  };
+
+  return (
+    <>
+      {token === "Test" ? (
+        <Header>
+          <Logo>
+            <Link href="/">FullDive</Link>
+          </Logo>
+          <Search>
+            <SearchBar placeholder="검색어를 입력해주세요" />
+            <RightBar>
+              {/* <div>
+                <img src="/images/more.svg" />
+                <img src="/images/notification.svg" />
+                <img src="/images/ranking.svg" />
+              </div> */}
+              <LoginButton onClick={onClickLogout}>로그아웃</LoginButton>
+            </RightBar>
+          </Search>
+        </Header>
+      ) : (
+        <Header>
+          <Logo>
+            <Link href="/">FullDive</Link>
+          </Logo>
+          <Search>
+            <SearchBar placeholder="검색어를 입력해주세요" />
+            <RightBar>
+              {/* <div>
+                <img src="/images/more.svg" />
+                <img src="/images/notification.svg" />
+                <img src="/images/ranking.svg" />
+              </div> */}
+              <Link href="/login">
+                <LoginButton>로그인</LoginButton>
+              </Link>
+            </RightBar>
+          </Search>
+        </Header>
+      )}
+    </>
+  );
+};
+
+const Header = styled.header`
+  position: fixed;
+  width: 100%;
+  height: 100px;
+  background-color: #181820;
+  display: flex;
+  color: #ffffff;
+  align-items: center;
+  z-index: 100;
+`;
+
+const Logo = styled.div`
+  min-width: 250px;
+  padding-left: 40px;
+  font-size: 1.5rem;
+  /* background: url("/images/logo.svg") no-repeat 15% 50%; */
+`;
+
+const Search = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SearchBar = styled.input`
+  background: url("/images/search.svg") no-repeat 20px 50% #21212b;
+  width: 360px;
+  border-radius: 100px;
+  border: none;
+  color: #ffffff;
+  font-size: 0.9rem;
+  font-weight: 400;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  padding: 0 55px;
+`;
+
+const RightBar = styled.div`
+  padding-right: 30px;
+  display: flex;
+  align-items: center;
+  > div {
+    > img {
+      margin-right: 20px;
+    }
+  }
+`;
+
+const LoginButton = styled.button`
+  width: 5rem;
+  background: #273dff;
+  border-radius: 100px;
+  font-size: 1rem;
+  margin-left: 10px;
+  padding: 10px;
+`;
+
+export default MainTop;
