6dca7f814dfc56684fbb061e603133bd25a3b240
tkddus614 <tkddus614@naver.com>
2023-03-06 17:52:37 +0900

    font family, images

--- fulldive_front/src/App.js
+++ fulldive_front/src/App.js
@@ -15,10 +15,6 @@ const persistor = persistStore(store);
 function App() {
   return (
     <>
-      {/* <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <title>FullDive</title>
-      </head> */}
       <BrowserRouter>
         <div className="App-content">
           <Provider store={store}>
